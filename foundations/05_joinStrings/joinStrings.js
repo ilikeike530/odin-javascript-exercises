af39ddae79cacb7037a174180179320a58b478f4
Isaac <21698003+ilikeike530@users.noreply.github.com>
2026-07-10 09:22:15 -0500

    Complete parts 01-05 of Odin javascript-exercises/foundations

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -1,11 +1,14 @@
-/*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+
+ // The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+
 
 // Add your code right below, good luck!
 
